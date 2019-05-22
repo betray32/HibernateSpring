192972283cfbdb3bfbadff39db6358b3f231905d
Camilo <contreras.camilo32@gmail.com>
2019-05-21 23:09:27 -0400

    Ajuste comentarios

--- Fuentes/HibernateSpring/src/main/java/cl/testing/dao/ConexionQueryDirecta.java
+++ Fuentes/HibernateSpring/src/main/java/cl/testing/dao/ConexionQueryDirecta.java
@@ -14,7 +14,8 @@ import cl.testing.HibernateTestingDao;
 import cl.testing.bean.Person;
 
 /**
- * Consultas para bases de datos con querys
+ * Consultas para bases de datos con querys , esto incluye conexiones directas a la base de datos mediante querys ,
+ * ademas se explica un ejemplo de parametros
  * 
  * @author ccontrerasc
  *
@@ -62,8 +63,11 @@ public class ConexionQueryDirecta {
 		String query = properties.getQUERY_PARAM();
 		log.info("Comenzando la ejecucion mediante Query Con Parametros");
 		log.info("Query : " + query);
+		
+		// Generar objeto con los parametros
+		Object[] params = { idPersona };
 
-		List<Person> listaSalida = jdbcTemplate.query(query, new Object[] { idPersona }, new BeanPropertyRowMapper<>(Person.class));
+		List<Person> listaSalida = jdbcTemplate.query(query, params, new BeanPropertyRowMapper<>(Person.class));
 		log.info("Lista consultada exitosamente");
 		if (listaSalida != null) {
 			for (Person p : listaSalida) {
